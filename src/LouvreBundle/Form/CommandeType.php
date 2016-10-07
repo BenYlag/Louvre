370a8622c54a484b63a8415e566274aa30ac46cd
BenY <beny.lagache@gmail.com>
2016-10-07 10:17:07 +0200

    Creation des formulaires pour Commande et Ticket

---
+++ src/LouvreBundle/Form/CommandeType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LouvreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('date', 'date')
+            ->add('payment')
+            ->add('duree')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LouvreBundle\Entity\Commande'
+        ));
+    }
+}
---
+++ src/LouvreBundle/Form/TicketType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LouvreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TicketType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('surname')
+            ->add('birth', 'date')
+            ->add('discount')
+            ->add('commande')
+            ->add('price')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LouvreBundle\Entity\Ticket'
+        ));
+    }
+}
