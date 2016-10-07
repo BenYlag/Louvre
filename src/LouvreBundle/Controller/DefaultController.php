4dd8df2a314cd209389a32aa12a09e12255dff4a
BenY <beny.lagache@gmail.com>
2016-10-07 09:11:13 +0200

    Suppression AppBundle et creation LouvreBundle

---
+++ src/LouvreBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LouvreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('LouvreBundle:Default:index.html.twig');
+    }
+}
--- src/LouvreBundle/LouvreBundle.php
+++ src/LouvreBundle/LouvreBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace LouvreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class LouvreBundle extends Bundle
 {
 }
