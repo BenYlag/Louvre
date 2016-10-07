a89500de4b92680ec12ad1bc2f87db2b452215e9
BenY <beny.lagache@gmail.com>
2016-10-07 12:17:28 +0200

    Creation et test formulaire de commande

--- src/LouvreBundle/Controller/LouvreController.php
+++ src/LouvreBundle/Controller/LouvreController.php
@@ -3,7 +3,10 @@
 namespace LouvreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use LouvreBundle\Entity\Commande;
+use LouvreBundle\Form\CommandeType;
 
 class LouvreController extends Controller
 {
@@ -18,8 +21,40 @@ class LouvreController extends Controller
     /**
      * @Route("/order")
      */
-    public function orderAction()
+    public function orderAction(Request $request)
     {
-        return $this->render('LouvreBundle:order:first.html.twig');
+        $commande = new Commande();
+        $form = $this->get('form.factory')->create(CommandeType::class,$commande);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $serial = $this
+                ->container
+                ->get('louvre.orderserial')
+                ->createSerial();
+            $commande->setName($serial);
+            $commande->setPayment('null');
+
+            $tickets = $form->get('tickets')->getData();
+            foreach ($tickets as $ticket) {
+                $price = $this
+                    ->container
+                    ->get('louvre.ticketprice')
+                    ->Pricing($ticket->getBirth(), $ticket->getDiscount());
+                $ticket->setCommande($commande);
+                $ticket->setPrice($price);
+            }
+
+            $em->persist($commande);
+            $em->flush();
+
+            return $this->render('LouvreBundle:order:first.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+
+        return $this->render('LouvreBundle:order:first.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
